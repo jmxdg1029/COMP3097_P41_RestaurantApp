06e9aa377436e8f7415bde8360fbd1d99ee5499d
Gabriel Silva <Gabriel.silva58@hotmail.com>
2022-04-21 15:06:39 -0400

    data and models added to the app folder

---
+++ src/app/storage/restaurants.ts
@@ -0,0 +1,23 @@
+import { Restaurant } from "../model/restaurant";
+
+let restaurants: Restaurant[] = [
+    {
+      id: 1,
+      name: "Gyubee",
+      address: "335 Bloor St W",
+      phoneNum: "4165519767",
+      description: "Best Japanese BBQ place",
+      tags: "Asian",
+      rating: 5
+    }, 
+
+    {
+        id: 2,
+        name: "The Burger's Priest",
+        address: "3280 Dufferin St",
+        phoneNum: "6473527070",
+        description: "They make some pretty good burgers",
+        tags: "American",
+        rating: 4
+      }
+]
